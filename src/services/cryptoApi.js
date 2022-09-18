b89a3b41de7a0374ba211c41c7af0cdf0e5825c8
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2022-09-18 12:37:07 +0500

    Basic layout, API, and Redux Toolkit added

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "8c02a4ee8emsh1bb27ccb1531b53p1e330ejsn5cd48d1eab30",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({}),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
